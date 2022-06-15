49d9d28a00b33b20e302ccdf3989d1e2aac469ec
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2022-06-15 15:16:42 -0300

    Create Atividade 4
    Create Task 4.

---
+++ aula-introducao/src/Components/Atividade_04/Data.js
@@ -0,0 +1,11 @@
+let Users = [
+    {_id: 0, name:"Jefferson de Carvalho", course: "Sistemas de Informação", ira: 6.7},
+    {_id: 1, name:"Juan David", course: "Engenharia de Computação", ira: 7.8},
+    {_id: 2, name:"Sarah Soares", course: "Sistemas de Informação", ira: 8.4},
+    {_id: 3, name:"Auri Joter", course: "Engenharia de Computação", ira: 7.2},
+    {_id: 4, name:"Daemon Salvatore", course: "Engenharia de Computação", ira: 9.7},
+    {_id: 5, name:"Stefan Salvatore", course: "Ciências da Computação", ira: 7.8},
+    {_id: 6, name:"Carlisle Cullin", course: "Sistemas de Informação", ira: 10},
+    {_id: 7, name:"Matt Rodrigues", course: "Engenharia de Computação", ira: 7.2}
+]
+//./mongod.exe --dbpath D:\UFC\GitHub\Desenvolvimento-de-Software-para-Web-2022.1---T03\aula-introducao\src\Components\Atividade_04\Data\
